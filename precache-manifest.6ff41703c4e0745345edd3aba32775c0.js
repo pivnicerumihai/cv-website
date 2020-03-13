76915bf363d07ab3c39637b1eb68809054ef204b
ML Pivniceru <mcpivniceru@gmail.com>
2020-03-13 18:29:30 +0000

    Updates

---
+++ precache-manifest.6ff41703c4e0745345edd3aba32775c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f264ebc686ece36126a70ff6fc5c55",
+    "url": "/pivnicerumihai/cv-website.git/index.html"
+  },
+  {
+    "revision": "0f324039beb32a02cec5",
+    "url": "/pivnicerumihai/cv-website.git/static/css/main.ed207b0f.chunk.css"
+  },
+  {
+    "revision": "07e665c4e08c3114b642",
+    "url": "/pivnicerumihai/cv-website.git/static/js/2.16e23f52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pivnicerumihai/cv-website.git/static/js/2.16e23f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f324039beb32a02cec5",
+    "url": "/pivnicerumihai/cv-website.git/static/js/main.68529889.chunk.js"
+  },
+  {
+    "revision": "458ab1437d8fda4d9449",
+    "url": "/pivnicerumihai/cv-website.git/static/js/runtime-main.1700c55a.js"
+  }
+]);
